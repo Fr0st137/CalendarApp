adba5715dfaf4c25b1c43840042016de331f7d16
Fr0st137 <tymkal137@gmail.com>
2024-06-05 18:26:36 +0200

    1st commit

---
+++ Date.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+
+std::string months[12] = {"January","February","March","April","May","June","July",
+                          "August","September","October","November","December"};
+
+int getDate(char dType);
---
+++ Date.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+int getDate(char dtype);
+
+std::string getDate();
+std::string getMonth();
+std::string getYear();
